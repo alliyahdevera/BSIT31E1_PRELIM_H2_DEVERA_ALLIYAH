04bb15ce7e0c4d2fcb45b92107c885dddd7c1591
alliyahdevera <alliyahalceldevera@gmail.com>
2026-06-24 12:57:24 +0800

    xml CS and sample files

--- ConsoleApp1/ConsoleApp1/XmlFileReader.cs
+++ ConsoleApp1/ConsoleApp1/XmlFileReader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace ConsoleApp1
 {
@@ -10,7 +11,23 @@ namespace ConsoleApp1
 
         protected override void ParseContent(string filePath)
         {
-            // code
+            Console.WriteLine(" -> Opening XML stream...");
+
+            string fullText = File.ReadAllText(filePath);
+            XDocument doc = XDocument.Load(filePath);
+
+            string rootName = doc.Root?.Name.LocalName ?? "unknown";
+            int nodeCount = doc.Descendants().Count();
+
+            Console.WriteLine($" -> Root element: <{rootName}> with {nodeCount} descendant nodes.");
+
+            string displayContent = fullText.Length > 250
+                ? fullText.Substring(0, 250) + "..."
+                : fullText;
+
+            Console.WriteLine("\n-------- First 250 Characters --------");
+            Console.WriteLine(displayContent);
+            Console.WriteLine("-------------------------------------\n");
         }
     }
 }
